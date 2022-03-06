daec4e20a7a6e218dbfa0436d04dd31fc4c8e59c
Railly <railly_2009@hotmail.com>
2022-03-06 14:17:16 -0500

    fix: remove unescaped literal

--- components/Markdown/Quote/index.js
+++ components/Markdown/Quote/index.js
@@ -1,9 +1,9 @@
 export default function Quote({ children }) {
   return (
-    <blockquote className="flex italic border-l-8 px-10 pt-5 pb-1 m-5 border-indigo-600 bg-indigo-50 dark:bg-gray-700">
-      <span>"</span>
+    <blockquote className="flex px-10 pt-5 pb-1 m-5 italic border-l-8 border-indigo-600 bg-indigo-50 dark:bg-gray-700">
+      <span>&ldquo;</span>
       {children}
-      <span>"</span>
+      <span>&ldquo;</span>
     </blockquote>
   );
 }
