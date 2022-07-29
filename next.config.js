a3bc5395b685a230f19914861d898a33f76ac64c
RaillyH <97618491+RaillyH@users.noreply.github.com>
2022-07-29 12:39:27 -0500

    fix: udpate nextjs 12.2.3

--- next.config.js
+++ next.config.js
@@ -6,4 +6,5 @@ module.exports = withContentlayer({
   // Append the default value with md extensions
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
   reactStrictMode: true,
+  swcMinify: true,
 });
