6032fcdb11cdf669975cc74180b7b9d980fabad8
Railly <railly_2009@hotmail.com>
2022-12-25 18:54:44 -0500

    fix: remove deprecated eslint rule

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @next/next/no-server-import-in-page */
 /* eslint-disable @next/next/no-img-element */
-// /pages/api/og.tsx
 
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
