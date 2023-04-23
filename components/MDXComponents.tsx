73aa834aa2b4dd6b4e74ea3b7018268febf03edb
Railly <railly_2009@hotmail.com>
2023-04-23 02:02:52 -0500

    fix: fix import in MDXComponent.tsx

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import Md from "components/markdown";
+import Md from "@/components/markdown";
 
 const CustomLink = (props: any) => {
   const href = props.href;
