817273a2941f2d09e4fc4e355683eb782c262086
Railly <railly_2009@hotmail.com>
2023-05-02 23:26:20 -0500

    fix: improve gap in section cards

--- components/cards/blog-card.tsx
+++ components/cards/blog-card.tsx
@@ -11,7 +11,7 @@ interface IBlogCardProps {
 const BlogCard = ({ blog }: React.PropsWithChildren<IBlogCardProps>) => {
   return (
     <Link
-      className="flex justify-between w-full h-full px-2 py-4 text-base border-t dark:border-white/20 hover:bg-[#efefef] border-black/20 dark:hover:bg-[#222222aa] transition-all"
+      className="flex gap-4 justify-between w-full h-full px-2 py-4 text-base border-t dark:border-white/20 hover:bg-[#efefef] border-black/20 dark:hover:bg-[#222222aa] transition-all"
       href={`/blog/${blog.slug}`}
       key={blog.slug}
     >
@@ -19,11 +19,11 @@ const BlogCard = ({ blog }: React.PropsWithChildren<IBlogCardProps>) => {
         {blog.title}
       </span>
       <div className="flex gap-7">
-        <span className="font-mono text-sm md:text-base">
+        <span className="font-mono text-sm text-center md:text-base">
           {blog.publishedAt && format(new Date(blog.publishedAt), "MMM, yyyy")}
         </span>
-        <span className="font-mono text-sm md:text-base">
-          <EyeIcon className="inline-block w-4 h-4 mr-2" />
+        <span className="inline-flex flex-col justify-center gap-2 font-mono text-center sm:flex-row sm:items-center">
+          <EyeIcon className="inline-block w-4 h-4" />
           {blog.views}
         </span>
       </div>
--- components/cards/project-card.tsx
+++ components/cards/project-card.tsx
@@ -10,18 +10,20 @@ interface IBlogCardProps {
 const ProjectCard = ({ project }: React.PropsWithChildren<IBlogCardProps>) => {
   return (
     <Link
-      className="flex justify-between w-full h-full px-2 py-4 text-base border-t dark:border-white/20 hover:bg-[#efefef] border-black/20 dark:hover:bg-[#222222aa] transition-all"
+      className="flex gap-4 justify-between w-full h-full px-2 py-4 text-base border-t dark:border-white/20 hover:bg-[#efefef] border-black/20 dark:hover:bg-[#222222aa] transition-all"
       href={`/project/${project.slug}`}
       key={project.slug}
     >
-      <span className="font-mono font-medium">{project.title}</span>
+      <span className="font-mono text-sm font-medium md:text-base">
+        {project.title}
+      </span>
       <div className="flex gap-7">
-        <span className="font-mono">
+        <span className="font-mono text-sm text-center md:text-base">
           {project.publishedAt &&
             format(new Date(project.publishedAt), "MMM, yyyy")}
         </span>
-        <span className="font-mono">
-          <EyeIcon className="inline-block w-4 h-4 mr-2" />
+        <span className="inline-flex flex-col justify-center gap-2 font-mono text-center sm:flex-row sm:items-center">
+          <EyeIcon className="inline-block w-4 h-4" />
           {project.views}
         </span>
       </div>
