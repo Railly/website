1ea4b63fa6121979563c8c5b0e4de96f01bf833a
Railly <railly_2009@hotmail.com>
2023-05-02 20:28:13 -0500

    fix: VIII

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,37 +1,8 @@
-import kv from "@vercel/kv";
 import { NextRequest, NextResponse } from "next/server";
 import postsData from "../../posts.json";
-import { getSortByPublishAt, getViewCount } from "../../../utils/helpers";
 
 export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
-  const url = new URL(req.url);
-  const sort = url.searchParams.get("sort");
-  const limit = url.searchParams.get("limit");
-
-  const views = await kv.hgetall<{
-    [key: string]: number;
-  }>("views");
-
-  if (!views) return NextResponse.json(postsData.posts);
-
-  const limitedPosts = limit
-    ? postsData.posts.slice(0, Number(limit))
-    : postsData.posts;
-
-  const sortFunction = getSortByPublishAt(sort);
-  const sortedPosts = sort ? limitedPosts.sort(sortFunction) : limitedPosts;
-
-  const posts = await Promise.all(
-    sortedPosts.map(async (post) => {
-      const viewsCount = await getViewCount(views, post.slug);
-      return {
-        ...post,
-        views: viewsCount,
-      };
-    })
-  );
-
-  return NextResponse.json(posts);
+  return NextResponse.json(postsData.posts);
 }
