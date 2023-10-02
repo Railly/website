94cce8432edbb2012ccf350ee9c1ffb653528f91
Railly <railly_2009@hotmail.com>
2023-10-02 01:35:42 -0500

    fix: remove eslint errors

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -2,5 +2,7 @@ import { NextResponse } from "next/server";
 import { getProjects } from "../../../lib/api/get-projects";
 
 export async function GET() {
-  return NextResponse.json(await getProjects());
+  return NextResponse.json(await getProjects({
+    limit: false
+  }));
 }
